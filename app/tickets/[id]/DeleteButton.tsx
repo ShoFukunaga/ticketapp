efcc30b02ae5556076a290341ead61f0fe12c0c7
ShoFukunaga <squall1tfjr@gmail.com>
2024-09-07 11:36:15 +0900

    no named commit

---
+++ app/tickets/[id]/DeleteButton.tsx
@@ -0,0 +1,65 @@
+"use client";
+import React, { useState } from "react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { buttonVariants } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+
+const DeleteButton = ({ ticketId }: { ticketId: number }) => {
+  const router = useRouter();
+  const [error, setError] = useState("");
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  const deleteTicket = async () => {
+    try {
+      setIsDeleting(true);
+      await axios.delete("/api/tickets/" + ticketId);
+      router.push("/tickets");
+      router.refresh();
+    } catch (error) {
+      setIsDeleting(false);
+      setError("UKnown Error Occured.");
+    }
+  };
+  return (
+    <div>
+      <AlertDialog>
+        <AlertDialogTrigger
+          className={buttonVariants({
+            variant: "destructive",
+          })}
+          disabled={isDeleting}
+          onClick={deleteTicket}
+        >
+          Delete Ticket
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your
+              ticket and remove your data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction>Delete</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+      <p className="text-destructive">{error}</p>
+    </div>
+  );
+};
+
+export default DeleteButton;
