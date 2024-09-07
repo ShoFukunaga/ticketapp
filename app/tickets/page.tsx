222a555c017da36ec3d0e248f9801a1411852b1a
ShoFukunaga <squall1tfjr@gmail.com>
2024-09-07 12:37:44 +0900

    no named commit

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -3,9 +3,48 @@ import prisma from "@/prisma/db";
 import DataTable from "./DataTable";
 import Link from "next/link";
 import { buttonVariants } from "@/components/ui/button";
+import Pagination from "@/components/Pagination";
+import StatusFilter from "@/components/StatusFilter";
+import { Status, Ticket } from "@prisma/client";
 
-const Tickets = async () => {
-  const tickets = await prisma.ticket.findMany();
+export interface SearchParams {
+  status: Status;
+  page: string;
+  orderBy: keyof Ticket;
+}
+
+const Tickets = async ({ searchParams }: { searchParams: SearchParams }) => {
+  const pageSize = 10;
+  const page = parseInt(searchParams.page) || 1;
+
+  const orderBy = searchParams.orderBy ? searchParams.orderBy : "createdAt";
+
+  const statuses = Object.values(Status);
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
+
+  let where = {};
+
+  if (status) {
+    where = {
+      status,
+    };
+  } else {
+    where = {
+      NOT: [{ status: "CLOSED" as Status }],
+    };
+  }
+  const ticketCount = await prisma.ticket.count({ where });
+  const tickets = await prisma.ticket.findMany({
+    where,
+    orderBy: {
+      [orderBy]: "desc",
+    },
+    take: pageSize,
+    skip: (page - 1) * pageSize,
+  });
+
   return (
     <div>
       <Link
@@ -14,7 +53,13 @@ const Tickets = async () => {
       >
         New Ticket
       </Link>
+      <StatusFilter />
       <DataTable tickets={tickets} />
+      <Pagination
+        itemCount={ticketCount}
+        pageSize={pageSize}
+        currentPage={page}
+      />
     </div>
   );
 };
